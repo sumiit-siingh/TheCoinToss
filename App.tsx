0531a6b297f83e1518ef0249df7bf13a3151e7e0
sumit singh <sumitsinghsakarari@gmail.com>
2025-06-27 10:06:20 +0530

    initial

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
+import { Text, View } from 'react-native';
+import { ScreenContent } from 'components/ScreenContent';
 import './global.css';
+
+function Header() {
+  return (
+    <View className="w-full bg-white py-4 px-6 shadow-md items-center">
+      <Text className="text-2xl font-bold text-blue-600">TheCoinToss</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <Header />
+      <ScreenContent title="" path="">
+        <Text className="text-xl text-blue-500">
+          Welcome to the Expo app with Tailwind CSS!
+        </Text>
+      </ScreenContent>
       <StatusBar style="auto" />
     </>
   );
--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
 export const EditScreenInfo = ({ path }: { path: string }) => {
-  const title = 'Open up the code for this screen:';
+  const title = '';
   const description =
-    'Change any of the text, save the file, and your app will automatically update.';
+    '';
 
   return (
     <View>
